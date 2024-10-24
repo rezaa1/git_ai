1864dd3dbb3c3bf55894c23da5dee87228f5bb37
reza_azimi <reza.azimi@inferencesolutions.com>
2024-10-24 19:14:23 +1100

    update fix frontned

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+console.log('API URL:', process.env.REACT_APP_API_URL);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
