44b4c9604023388cfe51dd9c1759d29640e7b029
reza_azimi <reza.azimi@inferencesolutions.com>
2024-10-24 14:57:45 +1100

    update add missing files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import CodeGenerator from './components/CodeGenerator';
-import RepositoryManager from './components/RepositoryManager';
-import TeamInfo from './components/TeamInfo';
 
 function App() {
   return (
     <div className="App">
-      <h1>AI Team Code Generator</h1>
       <CodeGenerator />
-      <RepositoryManager />
-      <TeamInfo />
     </div>
   );
 }
